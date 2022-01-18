f43c53fba20fa463f6a7f7f31b4ebb93a3bcee2d
bekri mehdi <bekri.mehdi@outlook.com>
2022-01-18 17:01:19 +0100

    test3

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 
-test("renders learn react header", () => {
+test("renders   react header", () => {
   render(<Header title="Macaron" />);
   const headingElement = screen.getByRole("heading");
   expect(headingElement).toBeInTheDocument();
