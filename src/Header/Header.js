e8a0d0e96227590515c5fdef989ad52690fa5e3e
bekri mehdi <bekri.mehdi@outlook.com>
2022-01-18 17:13:19 +0100

    test 2

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,9 @@
 import React from "react";
 export default function Header({ title }) {
-  return <h1>{title}</h1>;
+  return (
+    <>
+      <h1 className="header">{title}</h1>
+      <h3 className="header">Cats</h3>
+    </>
+  );
 }
--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -4,6 +4,12 @@ import Header from "./Header";
 
 test("renders   react header", () => {
   render(<Header title="Macaron" />);
-  const headingElement = screen.getByRole("heading");
+  const headingElement = screen.getByRole("heading", { name: "Cats" });
   expect(headingElement).toBeInTheDocument();
 });
+
+// test("render  react header", () => {
+//   render(<Header title2="Macarone" />);
+//   const headingElement = screen.getByRole("heading");
+//   expect(headingElement).toBeInTheDocument();
+// });
