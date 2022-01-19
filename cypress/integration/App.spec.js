d23feb7f8c445e7d09f1611cb6259ebb6d8efaf9
bekri mehdi <bekri.mehdi@outlook.com>
2022-01-19 15:34:44 +0100

    fix(feature/initialisation): delete french test and add cypress video to gitignore

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -2,7 +2,7 @@ describe("form sunmission", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
-  it("doit envoyer le formulaire", () => {
+  it("render form", () => {
     cy.get("#text").type("test");
     cy.get("#submitted").should("not.exist");
     cy.get("form").submit();
