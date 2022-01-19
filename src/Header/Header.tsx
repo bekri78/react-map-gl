51678eb1a18c7994960ca4ca988061d3f908c187
bekri mehdi <bekri.mehdi@outlook.com>
2022-01-19 12:01:41 +0100

    fix(feature/initialise): add test end to end

---
+++ src/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Header() {
+  return (
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Macaron
+    </a>
+  );
+}
