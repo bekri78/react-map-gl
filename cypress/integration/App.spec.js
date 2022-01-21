76a5753d0496f9abf9034cc43000628e179b124c
bekri mehdi <bekri.mehdi@outlook.com>
2022-01-21 18:47:49 +0100

    feat(feature/mapArrondissement): dysplay arrondisement from paris whith redux

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,6 +1,6 @@
 describe("map init", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000");
+    cy.visit("http://localhost:3001");
   });
   it("check map is load", () => {
     cy.get('div')
@@ -15,7 +15,7 @@ describe('naviagtion click on map', function() {
  
      cy.viewport(1600, 757)
   
-     cy.visit('http://localhost:3000/')
+     cy.visit('http://localhost:3001/')
   
      cy.get('.flex > .mapboxgl-map > div > div > .overlays').click()
   
